8e1b8dc1aa3576c0594131c665f25705878cbe15
Eduardo Vicente <eduardomcv42@gmail.com>
2023-10-27 21:39:12 +0100

    add checkout styles and layout

--- src/components/Checkout/Checkout.tsx
+++ src/components/Checkout/Checkout.tsx
@@ -1,8 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import { Button } from "../Button";
+
+import "./Checkout.scss";
+
 export function Checkout() {
+  const navigate = useNavigate();
+
+  function handleContinueShoppingClick() {
+    navigate("/");
+  }
+
+  function handlePayClick() {
+    navigate("/success");
+  }
+
   return (
     <>
-      <div>cart</div>
-      <button>asd</button>
+      <div className="order-container">
+        <h1>Order summary</h1>
+        <hr />
+      </div>
+      <div className="navigation-container">
+        <Button color="secondary" onClick={handleContinueShoppingClick}>
+          Continue shopping
+        </Button>
+        <Button onClick={handlePayClick}>Pay & collect</Button>
+      </div>
     </>
   );
 }
