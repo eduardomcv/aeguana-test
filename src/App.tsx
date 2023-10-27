e5c43839cca6a08307630a14d497cab7e3d8d785
Eduardo Vicente <eduardomcv42@gmail.com>
2023-10-27 19:11:34 +0100

    add checkout button

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,21 @@ import { Header } from "./components/Header";
 import { ProductsList } from "./components/ProductsList";
 
 import "./App.scss";
+import { Button } from "./components/Button";
 
 function App() {
+  function handleCheckoutClick() {
+    console.log("go to checkout");
+  }
+
   return (
     <ShoppingCartProvider>
       <Header />
       <main>
         <ProductsList />
+        <Button className="checkout-button" onClick={handleCheckoutClick}>
+          Go to checkout
+        </Button>
       </main>
     </ShoppingCartProvider>
   );
