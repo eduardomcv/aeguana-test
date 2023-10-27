40139ed8e338ed0c34dfa3397050a10dfff77163
Eduardo Vicente <eduardomcv42@gmail.com>
2023-10-27 15:17:56 +0100

    add index file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.scss";
-import { Header } from "./components/Header/Header";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <main></main>
     </div>
   );
 }
