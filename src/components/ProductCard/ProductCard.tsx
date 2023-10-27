6d070cac0e038fb3da6475e7bbf61a48f1870844
Eduardo Vicente <eduardomcv42@gmail.com>
2023-10-27 17:52:08 +0100

    format currency, add custom button and styles

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,7 +1,9 @@
-import { Product } from "../../products";
 import clsx from "clsx";
+import { Product } from "../../products";
+import { formatCurrency } from "../../utils";
 
 import "./ProductCard.scss";
+import { Button } from "../Button";
 
 interface ProductCardProps {
   product: Product;
@@ -19,8 +21,10 @@ export function ProductCard({ product }: ProductCardProps) {
       </div>
       <span className="product-name">{product.name}</span>
       <div className="price-container">
-        <span>€{product.price}</span>
-        <button>Add</button>
+        <span>{formatCurrency(product.price)}</span>
+        <Button className="add-button" disabled={product.isOutOfStock}>
+          Add
+        </Button>
       </div>
     </div>
   );
