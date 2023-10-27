1deec75808e62dca7717ad4ba03745c875c2d664
Eduardo Vicente <eduardomcv42@gmail.com>
2023-10-27 21:57:51 +0100

    fix list styles

--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -3,9 +3,11 @@ import { products } from "../../products";
 import { Button } from "../Button";
 import { ProductCard } from "../ProductCard";
 import "./ProductsList.scss";
+import { useShoppingCart } from "../../hooks/useShoppingCart";
 
 export function ProductsList() {
   const navigate = useNavigate();
+  const { shoppingCart } = useShoppingCart();
 
   function handleCheckoutClick() {
     navigate("/checkout");
@@ -13,14 +15,18 @@ export function ProductsList() {
 
   return (
     <>
-      <ul>
+      <ul className="products-list">
         {products.map((product) => (
           <li key={product.id}>
             <ProductCard product={product} />
           </li>
         ))}
       </ul>
-      <Button className="checkout-button" onClick={handleCheckoutClick}>
+      <Button
+        className="checkout-button"
+        onClick={handleCheckoutClick}
+        disabled={shoppingCart.size === 0}
+      >
         Go to checkout
       </Button>
     </>
