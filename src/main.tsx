5970ef7b104d6d3acb58a0ebc0b22dac0fb06cb9
Eduardo Vicente <eduardomcv42@gmail.com>
2023-10-27 16:59:34 +0100

    import font weights

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
 import "./index.scss";
-import "@fontsource/roboto";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/700.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
