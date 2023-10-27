f6fe8bc703c4c3631370643a1e68bef2124f0e18
Eduardo Vicente <eduardomcv42@gmail.com>
2023-10-27 23:54:12 +0100

    add feedback logic

--- src/components/Feedback/Feedback.tsx
+++ src/components/Feedback/Feedback.tsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+import { Star } from "../Star";
+
 import "./Feedback.scss";
 
 export function Feedback() {
-  return <div>stars</div>;
+  const [rating, setRating] = useState(0);
+
+  const stars = Array.from({ length: 5 }).map((_, index) => {
+    function handleFeedbackClick() {
+      setRating(index + 1);
+    }
+
+    return (
+      <button onClick={handleFeedbackClick}>
+        <Star filled={rating >= index + 1} />
+      </button>
+    );
+  });
+
+  return <div className="feedback-container">{stars}</div>;
 }
