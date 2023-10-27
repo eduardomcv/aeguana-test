5a8bf35510a9e18fc22f32e5d9bdff5c8fd7d0a5
Eduardo Vicente <eduardomcv42@gmail.com>
2023-10-27 23:09:49 +0100

    add styles

--- src/components/Success/Success.tsx
+++ src/components/Success/Success.tsx
@@ -1,14 +1,26 @@
+import { useShoppingCart } from "../../hooks/useShoppingCart";
+import { Fridge } from "../Fridge";
 import "./Success.scss";
 
 export function Success() {
+  const { shoppingCart } = useShoppingCart();
+
   return (
-    <div>
-      <h1>succ</h1>
-      <div>list</div>
-      <span>we love</span>
-      <div>stars</div>
-      <p>please collect</p>
-      <div>image</div>
+    <div className="success-background">
+      <div className="success-container">
+        <h1>Success</h1>
+        <ul className="shopping-cart">
+          {Array.from(shoppingCart).map(([id, amount]) => (
+            <li key={id}>asd</li>
+          ))}
+        </ul>
+        <p className="feedback">We would love your feedback</p>
+        <div>stars</div>
+        <p className="collect">Please collect your purchase</p>
+        <div>
+          <Fridge />
+        </div>
+      </div>
     </div>
   );
 }
